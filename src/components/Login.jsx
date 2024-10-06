921951fc1f227939a4e77bf74db83892c508ab9a
Tran Hau <hautran.01022002@gmail.com>
2024-10-06 20:45:28 +0700

    unit test

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Box, CardMedia } from "@mui/material";
 
 import { Videos, ChannelCard } from ".";
+import {
+  loginAPI,
+  loginAPIAsyncKey,
+  loginFacebookAPI,
+} from "../utils/fetchFromAPI";
+import { toast } from "react-toastify";
+import ReactFacebookLogin from "react-facebook-login";
 
 const Login = () => {
   const [channelDetail, setChannelDetail] = useState();
   const [videos, setVideos] = useState(null);
 
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {}, []);
 
@@ -30,9 +38,46 @@ const Login = () => {
             <input className="form-control" id="pass" />
           </div>
           <div className="col-12">
-            <button type="button" className="btn btn-primary">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() => {
+                let email = document.getElementById("email").value;
+                let pass_word = document.getElementById("pass").value;
+                console.log(email, pass_word);
+                loginAPIAsyncKey({ email, pass_word })
+                  .then((result) => {
+                    // pop up khi thnah cong
+                    toast.success(result.message);
+                    //luu accesss token trong local storage
+                    localStorage.setItem("LOGIN_USER", result.data.token);
+                    // chuyen huong sang tran chu khi thnah cong
+                    navigate("/");
+                  })
+                  .catch((error) => {
+                    // console.log("error API login");
+                    toast.error(error.response.data.message);
+                  });
+              }}
+            >
               Login
             </button>
+            <ReactFacebookLogin
+              appId="1085787246223971"
+              fields="name,email,picture"
+              callback={(response) => {
+                let { id, email, name } = response;
+                loginFacebookAPI({ id, email, name })
+                  .then((result) => {
+                    toast.success(result.message);
+                    localStorage.setItem("LOGIN_USER", result.data.token);
+                    navigate("/");
+                  })
+                  .catch((error) => {
+                    toast.error(error.response.data.message);
+                  });
+              }}
+            />
           </div>
         </form>
       </div>
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -2,41 +2,129 @@ import axios from "axios";
 
 export const BASE_URL = "http://localhost:8080";
 
-// const options = {
-//   params: {
-//     maxResults: 50,
-//   },
-//   headers: {
-//     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-//     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
-//     // 'token': localStorage.getItem("LOGIN_USER")
-//   },
-// };
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+    token: localStorage.getItem("LOGIN_USER"),
+  },
+};
+
+// tao mot instance cua axios
+export const axiosInstance = axios.create({
+  baseURL: `${BASE_URL}`,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // kiem tra flag
+    if (config.requireAuth) {
+      const accessToken = localStorage.getItem("LOGIN_USER");
+      if (accessToken) {
+        config.headers["token"] = `${accessToken}`;
+      }
+    }
+    return config;
+  },
+  (error) => {}
+);
+
+// config interceptors cho res moi khi res API tra ve 401
+
+const extendToken = async () => {
+  const { data } = await axiosInstance.post(
+    "/auth/extend-token",
+    {},
+    {
+      withCredentials: true, // cho phep gui va nhan cookie yu server
+    }
+  );
+  // luu accessToken moi vao localStorage
+  localStorage.setItem("LOGIN_USER", data.data);
+  return data;
+};
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  }, // param func khi res tra ve 2xx
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401) {
+      try {
+        const data = await extendToken();
+        console.log("data: ", data);
+        originalRequest.headers["token"] = data.data;
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        console.log("extend token failed", error);
+      }
+    }
+  } // param func khi api tra ve khac 2xx
+);
 
 export const fetchFromAPI = async (url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}`);
+  const { data } = await axiosInstance.get(`${BASE_URL}/${url}`);
 
   return data;
 };
 
 export const getListVideo = async () => {
-  const { data } = await axios.get(`${BASE_URL}/videos/get-videos`);
+  const { data } = await axiosInstance.get(`${BASE_URL}/videos/get-videos`);
   return data;
 };
 
 export const getType = async () => {
-  const { data } = await axios.get(`${BASE_URL}/videos/get-type`);
+  const { data } = await axiosInstance.get(
+    `${BASE_URL}/videos/get-type`,
+    {
+      requireAuth: true,
+    },
+    options
+  );
   return data;
 };
 
 export const getVideoById = async (typeId) => {
-  const { data } = await axios.get(
+  const { data } = await axiosInstance.get(
     `${BASE_URL}/videos/get-video-type-by-id/${typeId}`
   );
   return data;
 };
 
 export const registerAPI = async (payload) => {
-  const { data } = await axios.post(`${BASE_URL}/auth/register`, payload);
+  const { data } = await axiosInstance.post(
+    `${BASE_URL}/auth/register`,
+    payload
+  );
+  return data;
+};
+
+export const loginAPI = async (payload) => {
+  const { data } = await axiosInstance.post(`${BASE_URL}/auth/login`, payload, {
+    withCredentials: true, // cho phep gui va nhan cookie tu server BE
+  });
+  return data;
+};
+
+export const loginAPIAsyncKey = async (payload) => {
+  const { data } = await axiosInstance.post(
+    `${BASE_URL}/auth/login-async-key`,
+    payload,
+    {
+      withCredentials: true, // cho phep gui va nhan cookie tu server BE
+    }
+  );
+  return data;
+};
+
+export const loginFacebookAPI = async (newUser) => {
+  const { data } = await axiosInstance.post(
+    `${BASE_URL}/auth/login-face`,
+    newUser
+  );
   return data;
 };
